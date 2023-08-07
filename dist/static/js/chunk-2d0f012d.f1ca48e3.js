ae66cb7cd489887a34ec04c2185ac52c19f1ec69
张逸恒 <3044746799@qq.com>
2023-08-07 10:06:34 +0800

    使用手册

---
+++ dist/static/js/chunk-2d0f012d.f1ca48e3.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0f012d"],{"9b8f":function(e,r,n){"use strict";n.r(r);n("e0da"),n("96cd");var t,u,a={created:function(){var e=this.$route,r=e.params,n=e.query,t=r.path;this.$router.replace({path:"/"+t,query:n})},render:function(e){return e()}},c=a,o=n("7275"),p=Object(o["a"])(c,t,u,!1,null,null,null);r["default"]=p.exports}}]);
